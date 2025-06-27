d06350db8b84a8cb3e14a4f33432e98546a84e55
ta1355 <eorbs255@naver.com>
2025-06-28 05:34:10 +0900

    플레이어 점프 동작까지 완성

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -52,24 +52,46 @@ public class PlayerController : MonoBehaviour
 
     void Die()
     {
+        animator.SetTrigger("Die");
+
+        playerAudio.clip = deathClip;
+
+        playerAudio.Play();
+
+        // 사망시 속도를 0으로 변경
+        rigid.linearVelocity = Vector2.zero;
+
+        isDead = true;
 
     }
 
     // 트리거 콜라이터를 가진 장애물과의 출돌을 감지
     void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if (collision.tag == "Dead" && !isDead)
+        {
+            // 충돌한 상대방의 태그가 Dead이며 아직 사망하지 않는다면 Die함수 실행시키기
+            Die();
+        }
     }
 
     // 바닥에 닿았음을 감지하는 처리
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // 어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있으면
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
 
+            // isGrounded를 true로 변경, 누적 점프 횟수 0으로 초기화
+            isGrounded = true;
+            jumpCount = 0;
+        }
     }
 
     // 바닥에서 벗어났음을 감지하는 처리
     void OnCollisionExit2D(Collision2D collision)
     {
-        
+        // 어떤 콜라이더에서 떼어진 경우 isGrounded를 false로 변경
+        isGrounded = false;
     }
 }
