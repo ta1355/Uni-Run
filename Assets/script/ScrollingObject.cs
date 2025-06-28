26c5c3f3af521a344ef81987881c7411ca11d306
ta1355 <eorbs255@naver.com>
2025-06-29 06:46:38 +0900

    배경 레이어 순서 추가 및 배경 스크립트 추가

---
+++ Assets/script/ScrollingObject.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+  
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        // 초당 speed의 속도로 왼쪽으로 평행이동
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        
+    }
+}
